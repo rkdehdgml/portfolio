2bf52f9f5fca2aa5db04df95d4f13c9841f8baff
CEO <CEO@192.168.50.181>
2024-10-04 17:35:13 +0900

    컨텐츠가 한 세션 넘어가면 스크롤 추가

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -6,11 +6,12 @@ $(document).ready(function() {
 		keyboardScrolling: true,
 		css3: false,
 		autoScrolling: true,
-		afterLoad: function(origin, origin, direction) {
+		scrollOverflow: true,
+		afterLoad: function(origin, destination, direction) {
 			const header = document.getElementById('mainHeader');
 
 			// 현재 페이지를 확인
-			if (origin === 'page1') {
+			if (origin.anchor === 'page1') { // origin.anchor로 수정
 				header.style.transform = 'translateX(-100%)'; // 첫 번째 섹션에서 헤더 숨김
 			} else {
 				header.style.transform = 'translateX(0)'; // 나머지 섹션에서 헤더 표시
