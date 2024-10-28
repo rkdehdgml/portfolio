d6b141ebd581d08a2d91246cd49d4494436320c6
CEO <CEO@192.168.50.181>
2024-10-28 16:32:25 +0900

    카멜케이스 적용완

---
+++ src/main/java/com/springframework/portfolio/utils/MybatisMap.java
@@ -0,0 +1,15 @@
+package com.springframework.portfolio.utils;
+
+import java.util.HashMap;
+
+import org.springframework.jdbc.support.JdbcUtils;
+
+public class MybatisMap extends HashMap {
+	private static final long serialVersionUID = -7700790403928325865L;
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object put(Object key, Object value) {
+		return super.put(JdbcUtils.convertUnderscoreNameToPropertyName((String)key), value);
+	}
+}
